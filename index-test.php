ef8dd7bd63f6b5ff9814c4c27b2a56f5927f0dd7
rozzzly <rozzzly@gmail.com>
2013-03-04 19:57:13 -0600

    Installed yii bootstrap

--- index-test.php
+++ index-test.php
@@ -3,7 +3,7 @@
  * This is the bootstrap file for test application.
  * This file should be removed when the application is deployed for production.
  */
-
+date_default_timezone_set('America/Chicago');
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+date_default_timezone_set('America/Chicago');
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
